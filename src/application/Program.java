e1d8f1d3e1a347750cf3f748ee37a47d90693f4d
Vitor de Oliveira Lins <vitorlins485@gmail.com>
2026-01-08 00:21:30 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,27 @@
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
     static void main() {
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            IO.println();
+            IO.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            IO.println();
+            IO.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
+
     }
 }
