d506cc098f21aca71d8f27a167467bfa3e9b9d4e
Vitor de Oliveira Lins <vitorlins485@gmail.com>
2026-01-07 23:51:26 -0300

    Little improvement in board printing

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,17 +1,37 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
-            IO.print((8 - i) + " ");
+            IO.print(ANSI_RED + (8 - i) + " " + ANSI_RESET);
             for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j]);
             }
             IO.println();
         }
-        IO.println("  a b c d e f g h");
+        IO.println(ANSI_RED + "  a b c d e f g h" + ANSI_RESET);
     }
 
     public static void printPiece(ChessPiece piece) {
@@ -19,7 +39,12 @@ public class UI {
             IO.print("-");
         }
         else {
-            IO.print(piece);
+            if (piece.getColor() == Color.WHITE) {
+                IO.print(ANSI_WHITE + piece + ANSI_RESET);
+            }
+            else {
+                IO.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
         }
         IO.print(" ");
     }
