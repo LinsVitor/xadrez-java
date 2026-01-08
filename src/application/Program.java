0bfda903af8564367d167cfed3edd319066262e6
Vitor de Oliveira Lins <vitorlins485@gmail.com>
2026-01-07 21:51:30 -0300

    First class: Position

---
+++ src/application/Program.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+    static void main() {
+        Position pos = new Position(1, 3);
+        IO.println(pos);
+    }
+}
